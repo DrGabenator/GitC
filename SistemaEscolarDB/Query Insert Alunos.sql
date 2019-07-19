81728c28b15666c1bdd12edf190051fb00c7dfef
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 17:05:09 -0300

    Querys de Insert para todas as listas

---
+++ SistemaEscolarDB/Query Insert Alunos.sql
@@ -0,0 +1,9 @@
+﻿SET IDENTITY_INSERT [dbo].[Alunos] ON
+INSERT INTO [dbo].[Alunos] ([Id], [Nome], [Ativo]) VALUES (1, N'Gabriel', 1)
+INSERT INTO [dbo].[Alunos] ([Id], [Nome], [Ativo]) VALUES (2, N'Felipe', 1)
+INSERT INTO [dbo].[Alunos] ([Id], [Nome], [Ativo]) VALUES (4, N'João', 1)
+INSERT INTO [dbo].[Alunos] ([Id], [Nome], [Ativo]) VALUES (5, N'Guilherme', 1)
+INSERT INTO [dbo].[Alunos] ([Id], [Nome], [Ativo]) VALUES (6, N'Johnny', 1)
+INSERT INTO [dbo].[Alunos] ([Id], [Nome], [Ativo]) VALUES (7, N'Link', 1)
+INSERT INTO [dbo].[Alunos] ([Id], [Nome], [Ativo]) VALUES (8, N'Zelda', 1)
+SET IDENTITY_INSERT [dbo].[Alunos] OFF
---
+++ SistemaEscolarDB/Query Insert Turmas.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[Turmas] ON
+INSERT INTO [dbo].[Turmas] ([Id], [Nome], [Materia], [Ativo]) VALUES (1, N'300A', N'Matematica', 1)
+INSERT INTO [dbo].[Turmas] ([Id], [Nome], [Materia], [Ativo]) VALUES (2, N'300B', N'Matematica', 1)
+INSERT INTO [dbo].[Turmas] ([Id], [Nome], [Materia], [Ativo]) VALUES (3, N'201A', N'Portugues', 1)
+SET IDENTITY_INSERT [dbo].[Turmas] OFF
