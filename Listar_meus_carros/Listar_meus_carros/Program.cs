cc5b750939f051c6b48c4f2f27e8821bd30bbcf3
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 17:10:23 -0300

    Aula 29/07

---
+++ Listar_meus_carros/Listar_meus_carros/Program.cs
@@ -0,0 +1,67 @@
+﻿using Listar_meus_carros.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listar_meus_carros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Carro> minhaListaCarros = new List<Carro>();
+
+            while (true)
+            {
+                Console.WriteLine("Digite o número desejável para realizar a operação: ");
+                Console.WriteLine("1 - Listar os carros");
+                Console.WriteLine("2 - Adicionar carros a lista");
+                Console.WriteLine("3 - Sair");
+
+                int.TryParse(Console.ReadLine(), out int opcao);
+
+                switch (opcao)
+                {
+
+                    case 1:
+                        {
+                            foreach (Carro item in minhaListaCarros)
+                            {
+                                Console.WriteLine($"O modelo do carro é: {item.Modelo}\n" +
+                                    $"O ano do carro é: {item.Ano}\n" +
+                                    $"A placa do carro é: {item.Placa}\n" +
+                                    $"Os pulsões pugmáticos de potência do carro é de: {item.Cv}");
+                            }
+                            Console.ReadKey();
+                        }
+                        break;
+                    case 2:
+                        {
+                            minhaListaCarros.Add(new Carro()
+                            {
+                                Modelo = RetornaValores("Modelo"),
+                                Ano = int.Parse(RetornaValores("Ano")),
+                                Placa = RetornaValores("Valor"),
+                                Cv = int.Parse(RetornaValores("Pulsões pugmáticos de potência"))
+                            });
+                        }
+                        break;
+                    case 3:
+                        {
+                            return;
+                        }
+                }
+                Console.Clear();
+
+            }
+        }
+        public static string RetornaValores(string nome)
+        {
+            Console.WriteLine($"Informe o valor para o campo:{nome}");
+            return Console.ReadLine();
+
+        }
+    }
+}
---
+++ MinhaPrimeiraListaTipada/MinhaPrimeiraListaTipada/Program.cs
@@ -0,0 +1,67 @@
+﻿using MinhaPrimeiraListaTipada.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraListaTipada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //O indicador <T> o tipo da minha lista com isso temos uma lista de lanches
+            List<Lanche> minhaLista = new List<Lanche>();
+
+            //Adiciono na minha lista um pão de queijo
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite o nome do lanche: ");
+                var nome = Console.ReadLine();
+                Console.WriteLine("Digite a quantidade do lanche: ");
+                int.TryParse(Console.ReadLine(), out int quantidade);
+                Console.WriteLine("Digite o valor do lanche: ");
+                double.TryParse(Console.ReadLine(), out double valor);
+
+                minhaLista.Add(new Lanche()
+                {
+                    Nome = nome,
+                    Quantidade = quantidade,
+                    Valor = valor
+                });
+
+                Console.Clear();
+            }
+
+            //Aqui ando pela minha lista para poder apresentar em tela os valores
+            //item in significa que ele já é um índice da minha lista bonitinho
+            foreach (Lanche item in minhaLista)
+            {
+                Console.WriteLine($"Lanches disponíveis: {item.Nome}");
+            }
+
+            Console.WriteLine("Removendo item");
+
+            foreach (Lanche item in minhaLista)
+            {
+                if (item.Quantidade == 9)
+                {
+                    minhaLista.Remove(item);
+                    break;
+                }
+
+            }
+
+
+            foreach (Lanche item in minhaLista)
+            {
+                Console.WriteLine($"Lanches disponíveis: {item.Nome}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
